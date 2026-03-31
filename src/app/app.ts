e72bfb0c8af72f4e69bf8f2525e84186bc57a0f0
Mohamed Alaa <Mohamed@LAPTOP-JL1A13JG>
2026-03-31 14:55:49 +0200

    create login page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,26 @@
-import { Component, signal } from '@angular/core';
-import { Navbar } from "./shared/component/navbar/navbar";
-import { Footer } from "./shared/component/footer/footer";
-import { RouterOutlet } from "@angular/router";
+import { Component, signal, inject, computed } from '@angular/core';
+import { Navbar } from './shared/component/navbar/navbar';
+import { Footer } from './shared/component/footer/footer';
+import { RouterOutlet, Router, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { filter } from 'rxjs/operators';
+
 @Component({
   selector: 'app-root',
-  imports: [Navbar, Footer, RouterOutlet],
+  imports: [Navbar, Footer, RouterOutlet, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('HotelReservationSystem');
+  private router = inject(Router);
+  protected isLoginPage = signal(false);
+
+  constructor() {
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: any) => {
+        this.isLoginPage.set(event.urlAfterRedirects.includes('login'));
+      });
+  }
 }
--- src/app/components/auth/login/component/login/login.ts
+++ src/app/components/auth/login/component/login/login.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatIcon, MatButtonModule, RouterModule,CommonModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
-export class Login {
+export class Login implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor() {}
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm() {
+    this.loginForm = new FormGroup({
+      userName: new FormControl('',[Validators.required,]),
+      password: new FormControl('',[Validators.required,Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&]).{8,}$')]),
+    });
+  }
+
+  login(){
+
+  }
 }
